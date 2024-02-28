4fa8c8fe22b10ab81b92dd07bc300c5927c52037
LucasSM <lucas.silvamarra@gmail.com>
2024-02-28 17:04:49 -0300

    função adicionar montada

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 
+let listaAmigos = document.getElementById("lista-amigos");
+let campoAmigo = document.getElementById("nome-amigo");
+let amigos = [];
+
+function adicionar() {
+    let amigo = campoAmigo.value;
+    amigos.push(amigo);
+
+    if (listaAmigos.textContent == "") {
+        listaAmigos.textContent = amigo;
+    } else {
+        listaAmigos.textContent = listaAmigos.textContent + ", " + amigo;
+    }
+
+    campoAmigo.value = "";
+    campoAmigo.focus();
+    console.log(amigos);
+}
