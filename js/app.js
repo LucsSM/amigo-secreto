49538fa013cec76084e2d8e8f01e516130cb4aab
LucasSM <lucas.silvamarra@gmail.com>
2024-02-28 17:25:59 -0300

    função sortear montada

--- js/app.js
+++ js/app.js
@@ -1,19 +1,45 @@
 
-let listaAmigos = document.getElementById("lista-amigos");
 let campoAmigo = document.getElementById("nome-amigo");
+let listaAmigos = document.getElementById("lista-amigos");
 let amigos = [];
 
 function adicionar() {
     let amigo = campoAmigo.value;
     amigos.push(amigo);
-
+    
     if (listaAmigos.textContent == "") {
         listaAmigos.textContent = amigo;
     } else {
         listaAmigos.textContent = listaAmigos.textContent + ", " + amigo;
     }
-
+    
     campoAmigo.value = "";
     campoAmigo.focus();
-    console.log(amigos);
+}
+
+function sortear() {
+    let listaSorteio = document.getElementById("lista-sorteio");
+    embaralha(amigos);
+    
+    for(let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[i] + " --> " + amigos[0] + "<br>";
+        } else {
+            listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[i] + " --> " + amigos[i+1] + "<br>";
+        }
+    }
+}
+
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        
+        // guarda de um índice aleatório da lista
+        const elemento = lista[indice - 1];
+        
+        lista[indice - 1] = lista[indiceAleatorio];
+        
+        lista[indiceAleatorio] = elemento;
+    }
 }
